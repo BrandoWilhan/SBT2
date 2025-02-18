f3c4d6257921a468aecf1fcf97abba4a5901f316
Brando <pinikoskt@gmail.com>
2025-02-17 23:48:51 -0300

    adcionado trecho que verifica se o programa cabe nos espaços de memória

--- loader_counter.asm
+++ loader_counter.asm
@@ -1,32 +1,144 @@
 segment .data
 
-msg         db  "Hello World! Function", 0ah, 0
-MSG_SIZE    EQU $-msg
-buffer      times 12 db  0
-BUFFER_SIZE EQU $-buffer
+sem_espaco              db  "nao foi possivel carregar o programa na memoria, nao há espaco", 0ah, 0
+SEM_ESPACO_SIZE         EQU $-sem_espaco
+
+programa_carregado      db  "o programa foi carregado no(s) seguinte(s) par(es) <endereco> <mem_disponivel>", 0ah, 0
+PROGRAMA_CARREGADO_S    EQU $-programa_carregado
+
+espaco                  db  " ", 0
+ESPACO_S                EQU $-espaco
+
+nwln                  db  0ah
+NWLN_S                EQU $-nwln
+
+buffer      times 12    db  0
+BUFFER_SIZE             EQU $-buffer
 
 segment .text
 global f1
 ;EBP + 8 tamanho do programa
-;EBP + 8*i + 12 ; i = 0  até i = 3 ;; endereços de memória
-;EBP + 8*i + 16 ; i = 0  até i = 3 ;; memoria livre disponivel
+;EBP + 12 numero de pares de endereco memoria
+;EBP + 8*i + 16 ; i = 0  até i = 3 ;; endereços de memória
+;EBP + 8*i + 20 ; i = 0  até i = 3 ;; memoria livre disponivel
 
 f1:
     enter 0,0
-    mov eax, [EBP + 8]
+    mov esi, 0
+    mov ecx, [EBP + 12]
+    mov eax, 0
+
+.verifica_se_cabe:
+    cmp esi, 4
+    jae .verificado
+
+    mov edx, [EBP + 8*esi + 20] ; memorias
+    add eax, edx 
+    
+    inc esi
+    
+    loop .verifica_se_cabe
+
+cmp [EBP + 8], eax ;compara com tamanho do programa
+jg .nao_cabe
+
+
+;mostra acumulado
+
+mov edi, buffer
+call int_to_str
+push dword buffer
+push dword BUFFER_SIZE
+call print
+add esp, 8      ; esquece numero mostrado 
+
+call clear_buffer
+
+push dword espaco
+push dword ESPACO_S
+call print
+add esp, 8 
+push dword nwln
+push dword NWLN_S
+call print
+add esp, 8 
+
+.verificado:
+
+    
+    mov esi, 0
+    mov ecx, [EBP + 12]
+.mostra:
+    cmp esi, 4
+    jae .mostrado
+    ;mostra endereco
+    mov eax, [EBP + 8*esi + 16]
     mov edi, buffer
     call int_to_str
     push dword buffer
     push dword BUFFER_SIZE
     call print
-    add esp, 8
-    push dword msg  ; Empurrando o endereço diretamente para a pilha
-    push dword MSG_SIZE
+    add esp, 8      ; esquece numero mostrado 
+
+    call clear_buffer
+    ;mostra espaco
+    push dword espaco
+    push dword ESPACO_S
     call print
+    add esp, 8      ; esquece valor mostrado 
+
+    ;mostra memoria disponivel
+    mov eax, [EBP + 8*esi + 20]
+    mov edi, buffer
+    call int_to_str
+    push dword buffer
+    push dword BUFFER_SIZE
+    call print
+    add esp, 8      ; esquece numero mostrado 
+
+    call clear_buffer
+
+    ;mostra espaco
+    push dword espaco
+    push dword ESPACO_S
+    call print
+    add esp, 8      ; esquece valor mostrado 
+
+    inc esi
     
+    loop .mostra
+
+.mostrado:
+    ;mostra nwln
+    
+    push dword nwln
+    push dword NWLN_S
+    call print
+    add esp, 8
+
     leave
     ret
 
+.nao_cabe:
+    
+    push dword sem_espaco
+    push dword SEM_ESPACO_SIZE
+    call print
+    add esp, 8  
+    leave
+    ret
+
+clear_buffer:
+    enter 0,0
+    push ecx
+    mov ecx, BUFFER_SIZE  ; Número de bytes a limpar
+    mov edi, buffer       ; Aponta para o início do buffer
+     mov al, 0          ; Define o valor de limpeza como `0`
+    rep stosb             ; Limpa todo o buffer
+    pop ecx
+    leave
+    ret
+   
 print: 
     enter 0,0
     push eax
@@ -95,3 +207,4 @@ int_to_str:
     pop ecx
     pop ebx
     ret
+
