57bd1012d08a9451177feab9e9e99e86fc9c20d7
Brando <pinikoskt@gmail.com>
2025-02-19 16:24:14 -0300

    funcionalidade de verificar se o programa cabe e se couber em um espaco de memoria inteiro, mostra adcionada

--- loader_counter.asm
+++ loader_counter.asm
@@ -3,7 +3,7 @@ segment .data
 sem_espaco              db  "nao foi possivel carregar o programa na memoria, nao há espaco", 0ah, 0
 SEM_ESPACO_SIZE         EQU $-sem_espaco
 
-programa_carregado      db  "o programa foi carregado no(s) seguinte(s) par(es) <endereco> <mem_disponivel>", 0ah, 0
+programa_carregado      db  "o programa foi carregado nos seguintes par(es) <endereco_inicial> <endereco_final>", 0ah, 0
 PROGRAMA_CARREGADO_S    EQU $-programa_carregado
 
 espaco                  db  " ", 0
@@ -25,108 +25,117 @@ global f1
 f1:
     enter 0,0
     mov esi, 0
-    mov ecx, [EBP + 12]
+    mov ecx, [EBP + 12] ;numero de pares
     mov eax, 0
 
-.verifica_se_cabe:
-    cmp esi, 4
-    jae .verificado
+    .verifica_se_cabe:
 
-    mov edx, [EBP + 8*esi + 20] ; memorias
-    add eax, edx 
-    
-    inc esi
-    
-    loop .verifica_se_cabe
+        mov edx, [EBP + 8*esi + 20] ; memorias
+        add eax, edx 
+        cmp [EBP + 8], edx
+        je .iguais
 
-cmp [EBP + 8], eax ;compara com tamanho do programa
-jg .nao_cabe
+        inc esi
+        
+        loop .verifica_se_cabe
 
 
-;mostra acumulado
+    cmp [EBP + 8], eax ;compara com tamanho do programa
+    jg .nao_cabe
 
-mov edi, buffer
-call int_to_str
-push dword buffer
-push dword BUFFER_SIZE
-call print
-add esp, 8      ; esquece numero mostrado 
-
-call clear_buffer
-
-push dword espaco
-push dword ESPACO_S
-call print
-add esp, 8 
-push dword nwln
-push dword NWLN_S
-call print
-add esp, 8 
-
-.verificado:
-
-    
     mov esi, 0
-    mov ecx, [EBP + 12]
-.mostra:
-    cmp esi, 4
-    jae .mostrado
-    ;mostra endereco
-    mov eax, [EBP + 8*esi + 16]
-    mov edi, buffer
-    call int_to_str
-    push dword buffer
-    push dword BUFFER_SIZE
-    call print
-    add esp, 8      ; esquece numero mostrado 
+    mov ecx, [EBP + 12] ;numero de pares
 
-    call clear_buffer
-    ;mostra espaco
-    push dword espaco
-    push dword ESPACO_S
-    call print
-    add esp, 8      ; esquece valor mostrado 
 
-    ;mostra memoria disponivel
-    mov eax, [EBP + 8*esi + 20]
-    mov edi, buffer
-    call int_to_str
-    push dword buffer
-    push dword BUFFER_SIZE
-    call print
-    add esp, 8      ; esquece numero mostrado 
+    .mostra:
+        ;mostra endereco
+        mov eax, [EBP + 8*esi + 16]
+        mov edi, buffer
+        call int_to_str
+        push dword buffer
+        push dword BUFFER_SIZE
+        call print
+        add esp, 8      ; esquece numero mostrado 
 
-    call clear_buffer
+        call clear_buffer
+        ;mostra espaco
+        push dword espaco
+        push dword ESPACO_S
+        call print
+        add esp, 8      ; esquece valor mostrado 
 
-    ;mostra espaco
-    push dword espaco
-    push dword ESPACO_S
-    call print
-    add esp, 8      ; esquece valor mostrado 
+        ;mostra memoria disponivel
+        mov eax, [EBP + 8*esi + 20]
+        mov edi, buffer
+        call int_to_str
+        push dword buffer
+        push dword BUFFER_SIZE
+        call print
+        add esp, 8      ; esquece numero mostrado 
 
-    inc esi
+        call clear_buffer
+        ;mostra espaco
+        push dword espaco
+        push dword ESPACO_S
+        call print
+        add esp, 8      ; esquece valor mostrado
+        push dword nwln
+        push dword NWLN_S
+        call print
+        add esp, 8
+
+        inc esi
+        
+        loop .mostra
+
     
-    loop .mostra
+    .iguais:
+        call mostrar_msg_carregado
+        mov ebx,[EBP + 8*esi + 16]  ;endereco inicial
+        ;mostrar inicial
+        mov eax, ebx
+        mov edi, buffer
+        call int_to_str
+        push dword buffer
+        push dword BUFFER_SIZE
+        call print
+        add esp, 8      ; esquece numero mostrado 
 
-.mostrado:
-    ;mostra nwln
+        dec edx
+
+        call clear_buffer
+        ;mostra espaco
+        push dword espaco
+        push dword ESPACO_S
+        call print
+        add esp, 8      ; esquece valor mostrado 
+
+        add ebx, edx
+        ;mostrar end final
+        mov eax, ebx
+        mov edi, buffer
+        call int_to_str
+        push dword buffer
+        push dword BUFFER_SIZE
+        call print
+        add esp, 8
+        ;nwln
+        push dword nwln
+        push dword NWLN_S
+        call print
+        add esp, 8
+        jmp .return    
+
+    .nao_cabe:
+        
+        push dword sem_espaco
+        push dword SEM_ESPACO_SIZE
+        call print
+        add esp, 8  
     
-    push dword nwln
-    push dword NWLN_S
-    call print
-    add esp, 8
-
-    leave
-    ret
-
-.nao_cabe:
-    
-    push dword sem_espaco
-    push dword SEM_ESPACO_SIZE
-    call print
-    add esp, 8  
-    leave
-    ret
+    .return:
+        leave
+        ret
 
 clear_buffer:
     enter 0,0
@@ -148,7 +157,7 @@ print:
 
     mov eax, 4          ;chamada de sistema syswrite
     mov ebx, 1          ;stdout
-    mov ecx, [EBP + 12] ;msg
+    mov ecx, [EBP + 12] ;numero de pares ;msg
     mov edx, [EBP + 8]  ;msg_size
     int 80h
 
@@ -173,38 +182,49 @@ int_to_str:
     jge .loop            ; Se for positivo, continua normalmente
 
 
-.loop:
-    xor edx, edx         ; Limpa EDX antes da divisão
-    div ecx              ; Divide EAX por 10, resto em EDX (dígito extraído)
-    add dl, '0'          ; Converte número para ASCII
-    mov [edi], dl        ; Armazena no buffer
-    inc edi              ; Avança para próxima posição
+    .loop:
+        xor edx, edx         ; Limpa EDX antes da divisão
+        div ecx              ; Divide EAX por 10, resto em EDX (dígito extraído)
+        add dl, '0'          ; Converte número para ASCII
+        mov [edi], dl        ; Armazena no buffer
+        inc edi              ; Avança para próxima posição
 
-    test eax, eax        ; Se EAX == 0, terminou a conversão
-    jnz .loop
+        test eax, eax        ; Se EAX == 0, terminou a conversão
+        jnz .loop
 
-    mov byte [edi], 0    ; Adiciona terminador nulo (`\0`)
-    dec edi              ; `edi` agora aponta para o último caractere válido
+        mov byte [edi], 0    ; Adiciona terminador nulo (`\0`)
+        dec edi              ; `edi` agora aponta para o último caractere válido
 
-.reverse:
-    cmp esi, edi         ; Se `esi >= edi`, terminamos a inversão
-    jge .done
+    .reverse:
+        cmp esi, edi         ; Se `esi >= edi`, terminamos a inversão
+        jge .done
 
-    mov al, [esi]        ; Troca caracteres
-    mov ah, [edi]
-    mov [esi], ah
-    mov [edi], al
+        mov al, [esi]        ; Troca caracteres
+        mov ah, [edi]
+        mov [esi], ah
+        mov [edi], al
 
-    inc esi              ; Move início para frente
-    dec edi              ; Move fim para trás
-    jmp .reverse
+        inc esi              ; Move início para frente
+        dec edi              ; Move fim para trás
+        jmp .reverse
 
-.done:
-    mov eax, buffer 
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
+    .done:
+        mov eax, buffer 
+        pop edi
+        pop esi
+        pop edx
+        pop ecx
+        pop ebx
+        ret
+
+mostrar_msg_carregado:
+    enter 0,0
+    
+
+    push dword programa_carregado
+    push dword PROGRAMA_CARREGADO_S
+    call print
+    add esp, 8      ; esquece valor mostrado
+
+    leave
     ret
-
